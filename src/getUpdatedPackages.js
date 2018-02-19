9560a371a4af726a23196673aa4d0e9afb9e1a8f
Jacob LeGrone <git@jacob.work>
2018-02-19 11:37:45 -0500

    feat: surface valid lerna errors

--- src/getUpdatedPackages.js
+++ src/getUpdatedPackages.js
@@ -1,12 +1,16 @@
 import { execSync } from 'child_process';
 
-export default function getUpdatedPackages(options) {
+export default function getUpdatedPackages(options = {}) {
   const args = options.arguments || [];
   try {
     return JSON.parse(
       execSync(`lerna updated ${args.join(' ')} --json`, { stdio: 'pipe' }).toString()
     );
   } catch(e) {
-    return [];
+    if (e.stderr.toString().includes('No packages')) {
+      return [];
+    } else {
+      throw e;
+    }
   }
 }
